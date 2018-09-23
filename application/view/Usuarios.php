f89918804596db977fabd5af2e1732b40a2656c4
Geovanny Saldarriaga <geosar1@hotmail.com>
2018-09-23 14:17:41 -0500

    Cambios modulo usuarios

--- application/view/Usuarios.php
+++ application/view/Usuarios.php
@@ -4,12 +4,12 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
                 <label>Nombres</label>
                 <input id="nombres" class="form-control" name="txtnombres" type="text" maxlength="45" autocomplete="off" required>
             </div>
 
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
                 <label>Apellidos</label>
                 <input id="apellidos" class="form-control" name="txtapellidos" type="text" maxlength="45" autocomplete="off" required>
             </div>
@@ -18,7 +18,7 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
                 <label>Tipo de documento</label>
                 <select id="tipo_doc" class="form-control" name="txttipo_doc" required>
                     <option value="">Seleccione</option>
@@ -27,11 +27,11 @@
                     <option value="Tarjeta">Tarjeta Identidad</option>
                 </select>
             </div>
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
                 <label>Número documento</label>
                 <input name="txtnumero_doc" class="form-control" id="num_doc" type="text" maxlength="30" autocomplete="off" required>
             </div>
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
                 <label>Fecha Expedición</label>
                 <input name="txtfec_exp" class="form-control" id="fec_exp" type="date" required>
             </div>
@@ -40,7 +40,7 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
                 <label>Tipo de Usuario</label>
                 <select id="tipo_usu" class="form-control" name="txttipo_usu" required>
                     <option value="ADMINISTRADOR">Administrador</option>
@@ -48,18 +48,19 @@
                     <option value="BODEGA">Bodega</option>
                 </select>
             </div>
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
                 <label>Usuario</label>
                 <input id="user" class="form-control" name="txtuser" type="text" maxlength="30" autocomplete="off" required>
+            </div>
+            <div class="col-md-4 form-group">
+                <label>Contraseña</label>
+                <input id="clave" class="form-control" name="txtclave" type="text" maxlength="32" autocomplete="off" required>
             </div>
         </div>
     </div>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 form-group">
-                <label>Contraseña</label>
-                <input id="clave" class="form-control" name="txtclave" type="text" maxlength="32" autocomplete="off" required>
-            </div>
+
         </div>
     </div>
 
@@ -67,7 +68,7 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 form-group">
+            <div class="col-md-4 form-group">
 
                 <input class="succes" id="guardar_usuario" type="submit" value="Guardar" formaction="<?= URL ?>usuario/guardar">
                 <input class="succes" id="modificar_usuario" type="submit" value="Guardar cambios" formaction="<?= URL ?>usuario/modificar">
@@ -92,7 +93,7 @@
         <div class="modal-body">
             <label>Ingrese la nueva Contraseña</label>
             <input id="cambiopass" class="form-control" name="txtcambiopass" type="text" maxlength="32" autocomplete="off" required> <br>
-            <input class="succes" id="btcambiarpass" type="button" value="Modificar">
+            <input class="succes" id="btcambiarpass" type="button" value="Modificar"><br>
         </div>
         <div id="footer-modal">
             <button id="volverpass" type="button">Cerrar</button>
