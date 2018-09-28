7d2fc764ff14f3325fe6474757f2021aad5eea31
Geovanny Saldarriaga <geosar1@hotmail.com>
2018-09-28 14:48:24 -0500

    Ingreso de contenido modulo ayuda

--- application/view/ayuda.php
+++ application/view/ayuda.php
@@ -1,3 +1,68 @@
-<form>
+<form id="ayuda_online" action="" method="post">
+    <h2 id="titulo">Ayuda</h2>
+    <br>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 form-group">
+                 <h5>Esta sección está dedicada a brindar ayuda a través de documentos en formato PDF, donde podrás ubicar en primer lugar el nombre del módulo que deseas conocer, después de dar clic en el enlace, se abrirá en otra página el documento y paso seguido navegaras dentro del documento buscando la información requerida. </h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-md-12 form-group">
+                <a>Modulo de Usuarios.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a>Modulo de Clientes.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a>Modulo de Proveedores.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a>Modulo de Productos.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+               <a>Modulo de Cartera.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a>Modulo de Compras.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a>Modulo de Pedidos.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a>Modulo de Rutas.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a>Modulo de Movimientos.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+               <a>Modulo de Reportes.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                 <h5>Esperamos que esta información te sea de gran ayuda, en caso contrario, no dudes en contactar al administrador del sistema.</h5>
+            </div>
+            
+        </div>
+    </div>
     
+
+    
+
+
+
 </form>
