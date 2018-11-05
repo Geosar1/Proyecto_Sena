a3955b3a0666720c319184ac56ad639fd1ef2b5a
Geovanny Saldarriaga <geosar1@hotmail.com>
2018-11-05 18:13:11 -0500

    Visualizar ayudas PDF

--- application/view/ayuda.php
+++ application/view/ayuda.php
@@ -15,43 +15,55 @@
         <div class="row">
 
             <div class="col-md-12 form-group">
-                <a>Modulo de Usuarios.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/01.INGRESO AL SISTEMA.pdf" target="_blank">Ingreso al sistema.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Clientes.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/02.MODULO DE USUARIOS.pdf" target="_blank">Modulo de Usuarios.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Proveedores.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/03.MODULO DE CLIENTES.pdf" target="_blank">Modulo de Clientes.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Productos.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/04.MODULO PROVEEDORES.pdf" target="_blank">Modulo de Proveedores.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Cartera.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/05.MODULO PRODUCTOS.pdf" target="_blank">Modulo de Productos.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Compras.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/06.MODULO CARTERA.pdf" target="_blank">Modulo de Cartera.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Pedidos.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/07.MODULO COMPRAS.pdf" target="_blank">Modulo de Compras.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Rutas.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/08.MODULO PEDIDOS.pdf" target="_blank">Modulo de Pedidos.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Movimientos.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/09.MODULO RUTAS.pdf" target="_blank">Modulo de Rutas.</a>
             </div>
             <br>
             <div class="col-md-12 form-group">
-                <a>Modulo de Reportes.</a>
+                <a href="<?= URL ?>/public/Documentos_pdf/10.MODULO MOVIMIENTOS.pdf" target="_blank">Modulo de Movimientos.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a href="<?= URL ?>/public/Documentos_pdf/11.MODULO REPORTES.pdf" target="_blank">Modulo de Reportes.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a href="<?= URL ?>/public/Documentos_pdf/12.MODULO AYUDA EN LINEA.pdf" target="_blank">Ayuda en linea.</a>
+            </div>
+            <br>
+            <div class="col-md-12 form-group">
+                <a href="<?= URL ?>/public/Documentos_pdf/13.GLOSARIO.pdf" target="_blank">Glosario.</a>
             </div>
         </div>
     </div>
